7922de6d4d0cf2d71655ec47100ef2b4d654ddb8
zhouL <lz.mms@foxmail.com>
2022-10-18 15:56:43 +0800

    完善图鉴列表页

--- app/src/main/java/com/lodz/android/pokemondex/ui/pokedex/PokemonListAdapter.kt
+++ app/src/main/java/com/lodz/android/pokemondex/ui/pokedex/PokemonListAdapter.kt
@@ -3,18 +3,14 @@ package com.lodz.android.pokemondex.ui.pokedex
 import android.content.Context
 import android.graphics.*
 import android.view.ViewGroup
-import android.widget.ImageView
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.github.florent37.glidepalette.BitmapPalette
 import com.github.florent37.glidepalette.GlidePalette
 import com.google.android.material.imageview.ShapeableImageView
-import com.lodz.android.corekt.anko.dp2pxRF
-import com.lodz.android.corekt.anko.getColorCompat
+import com.lodz.android.corekt.anko.startRotateSelf
 import com.lodz.android.imageloaderkt.ImageLoader
-import com.lodz.android.pandora.utils.coroutines.CoroutinesWrapper
 import com.lodz.android.pandora.widget.rv.recycler.vh.DataVBViewHolder
 import com.lodz.android.pandora.widget.rv.tree.BaseTreeRvAdapter
-import com.lodz.android.pokemondex.R
 import com.lodz.android.pokemondex.bean.poke.pkm.PkmGenBean
 import com.lodz.android.pokemondex.bean.poke.pkm.PkmInfoBean
 import com.lodz.android.pokemondex.databinding.RvItemGenBinding
@@ -60,12 +56,15 @@ class PokemonListAdapter(context: Context) : BaseTreeRvAdapter<PkmGenBean, DataV
     private fun showGenUI(holder: DataVBViewHolder, bean: PkmGenBean) {
         holder.getVB<RvItemGenBinding>().apply {
             genTv.text = bean.getGenStr()
-            draArrowImg(arrowImg)
+            if (bean.isExpandItem()) {
+                arrowImg.startRotateSelf(0, 90, 100, true)
+            }
         }
     }
 
     private fun showPkmUI(holder: DataVBViewHolder, bean: PkmInfoBean) {
         holder.getVB<RvItemPokemonBinding>().apply {
+            idTv.text = bean.index
             pokeImg.setBackgroundColor(Color.LTGRAY)
             showImg(pokeImg, bean.imgUrl)
         }
@@ -87,39 +86,5 @@ class PokemonListAdapter(context: Context) : BaseTreeRvAdapter<PkmGenBean, DataV
                     }
                     .crossfade(true)
             )
-    }
-
-    /** 绘制箭头图标 */
-    private fun draArrowImg(img: ImageView) {
-        CoroutinesWrapper.create(this)
-            .request { getArrowBitmap() }
-            .action { onSuccess { img.setImageBitmap(it) } }
-    }
-
-    /** 获取箭头图片*/
-    private fun getArrowBitmap(): Bitmap {
-        val side = context.dp2pxRF(20)
-
-        val centerPoint = side / 2
-
-        val bitmap = Bitmap.createBitmap(side.toInt(), side.toInt(), Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        canvas.drawColor(Color.TRANSPARENT)
-        val paint = Paint()
-        paint.color = context.getColorCompat(R.color.color_909090)
-        paint.strokeWidth = 7f
-        paint.isAntiAlias = true
-        paint.style = Paint.Style.STROKE
-        val path = Path()
-        path.moveTo(centerPoint, centerPoint + context.dp2pxRF(3))
-        path.lineTo(centerPoint - context.dp2pxRF(7), centerPoint - context.dp2pxRF(3))
-        path.moveTo(centerPoint, centerPoint + context.dp2pxRF(3))
-        path.lineTo(centerPoint + context.dp2pxRF(7), centerPoint - context.dp2pxRF(3))
-        canvas.drawPath(path, paint)
-
-        paint.isAntiAlias = true
-        paint.style = Paint.Style.FILL
-        canvas.drawCircle(centerPoint, centerPoint + context.dp2pxRF(3), 3f, paint)
-        return bitmap
     }
 }
